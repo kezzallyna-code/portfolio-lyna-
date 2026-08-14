3d40e6c83a589ff392b4efb694e011969ead3a20
kezzallyna-code <kezzallyna-code@users.noreply.github.com>
2026-08-14 03:13:20 +0100

    chore: clean up test files

--- test-get.ts
+++
@@ -1,7 +0,0 @@
-import { getPortfolioData } from './src/data/repository';
-
-async function test() {
-  const data = await getPortfolioData();
-  console.log(data.projects.map(p => p.id));
-}
-test();
